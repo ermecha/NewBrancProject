c536193a5cbe7625bb50ac5ad8b225b95ce16200
ermek <sagynbekoff@icloud.com>
2022-04-02 12:11:32 -0500

    new one

---
+++ src/test_cases/TC002.java
@@ -0,0 +1,9 @@
+package test_cases;
+
+import javax.management.MBeanAttributeInfo;
+
+public class TC002 {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
