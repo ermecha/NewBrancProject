6112c10e59f70c1d025063d7868f2a16446dab22
ermek <sagynbekoff@icloud.com>
2022-04-06 18:45:10 -0500

    loop 2 created

---
+++ src/test_cases/Problem.java
@@ -0,0 +1,10 @@
+package test_cases;
+
+public class Problem {
+    public static void main(String[] args) {
+        for (int i = 0; i < 6; i++) {
+            System.out.println(i);
+
+        }
+    }
+}
